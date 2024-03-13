baa24383588d234bfabadedd54e4f6ee14b4a81e
clackclock <clackclick2020@gmail.com>
2024-03-13 18:36:48 -0400

    lol

--- extension/popup.js
+++ extension/popup.js
@@ -1,5 +1,27 @@
 console.log("it works");
 
-$(document).ready(function(){
-    $("body").css("background-color", "antiquewhite");
-})
+let article = document.querySelector("article");
+
+if(article){
+    let text = article.textContent;
+    let wordMatch = /[^\s]+/g;
+    let words = text.matchAll(wordMatch);
+
+    let wordCount = [...words].length;
+    let readingTime = Math.round(wordCount / 200);
+    let badge = document.createElement("p");
+
+    badge.textContent = "⏰: " + readingTime + " minute read";
+    let firstH1 = article.querySelector("h1");
+    if(firstH1){
+        firstH1.append(badge);
+    }else{
+        let body = document.body;
+        body.prepend(badge);
+    }
+
+}
+
+// $(document).ready(function(){
+//     $("body").css("background-color", "antiquewhite");
+// })
